1db90335b53406b7eece0859aa40eca42e9e2b4f
KillaGoose51 <andrew.maxey825@gmail.com>
2023-05-02 13:54:17 -0500

    Initial commit

---
+++ Constructors_Maxey_Andrew/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Maxey_Andrew
+{
+    class Game
+    {
+
+        public string title, genre;
+
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {
+            title = titleParam;
+            genre = genreParam;
+        }
+
+    }
+}
---
+++ Constructors_Maxey_Andrew/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors_Maxey_Andrew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Game = Datatype MyGame = name of the datatype, new = new object Game is type of object// Without manual input it will be unknown/ default
+            Game myGame = new Game("Minecraft", "Open World");
+            // myGame is the object and .title is a properity/variable to the object that can be pulled/called
+            Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!");
+
+            Game myOtherGame = new Game(":)",":(");
+
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game!");
+        }
+    }
+}
